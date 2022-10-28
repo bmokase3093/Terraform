d45e6faa156750560c70011f279299eff0b407df
bmokase3093 <bmokase3093@gmail.com>
2022-10-28 16:21:40 +0200

    depends on

--- Dependencies/main.tf
+++ Dependencies/main.tf
@@ -1,15 +1,17 @@
 provider "aws" {
-    region = "eu-west-2"
+    region = "eu-central-1"
 }
 
-resource "aws_instance" "db" {
-    ami = "ami-0caef02b518350c8b"
-    instance_type = "t2.micro"
+resource "aws_instance" "UbuntuEC2" {
+  ami = "ami-0caef02b518350c8b"
+  instance_type = "t2.micro"
 }
 
 resource "aws_instance" "web" {
-    ami = "ami-0caef02b518350c8b"
+    ami = "ami-06148e0e81e5187c8"
     instance_type = "t2.micro"
 
-    depends_on = [aws_instance.db]
+    depends_on = [
+      aws_instance.UbuntuEC2
+    ]
 }
